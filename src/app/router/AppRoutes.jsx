4e53bce9f303eb99c2505f44c99299a69d2d83dc
SuarezGil <isuarez-2021691@kinal.edu.gt>
2026-04-06 17:19:46 -0600

    primer avance de js con html

---
+++ src/app/router/AppRoutes.jsx
@@ -0,0 +1,11 @@
+import {Routes, Route} from 'react-router-dom'
+import { AuthPage } from '../../features/auth/pages/AuthPage'
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={ <AuthPage /> } />
+    </Routes>
+  )
+
+}
---
+++ src/features/auth/components/LoginForm.jsx
@@ -0,0 +1,20 @@
+
+
+export const LoginForm = () => {
+  return (
+    
+    <form action="" className="space-y-5">
+        <div>
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700">Correo Electrónico
+
+            </label>
+            <input 
+            id=""
+            type="text"
+            placeholder="correo@ejemplo.com o usuario" 
+            className="w-full px-3 py-2 text-sm border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"/>
+            
+            </div>
+    </form>
+  )
+}
