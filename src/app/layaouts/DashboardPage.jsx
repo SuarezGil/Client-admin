4a910bbb80447fbe2051e2f29d42f8c65d2441d9
SuarezGil <isuarez-2021691@kinal.edu.gt>
2026-04-14 17:26:51 -0600

    Se implemento un Navbar

---
+++ src/app/layaouts/DashboardPage.jsx
@@ -0,0 +1,7 @@
+import { DashboardContainer } from "../../shared/layaout/DashboardContainer"
+
+export const DashboardPage = () => {
+  return (
+    <DashboardContainer></DashboardContainer>
+  )
+}
